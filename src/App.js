68af4b864d042cc735d4faa57ef93a4a50c657c3
tadrochlinski <tadrochlinski@gmail.com>
2022-08-29 14:32:37 +0200

    google fonts added, global styles created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
+import GlobalStyles from './Components/GlobalStyles';
 
 const App = () =>{
   return (
-      <Routes>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/register" element={<Register/>}></Route>
-        <Route path="/" element={<Home/>}></Route>
-      </Routes>
+      <>
+        <GlobalStyles/>
+        <Routes>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/register" element={<Register/>}></Route>
+          <Route path="/" element={<Home/>}></Route>
+        </Routes>
+      </>
   );
 }
 
