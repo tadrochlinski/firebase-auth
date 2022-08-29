d7ff262ebe471da040545af0202cbfd0727a920a
tadrochlinski <tadrochlinski@gmail.com>
2022-08-29 14:24:04 +0200

    router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+
 const App = () =>{
-  return <h1>Hello</h1>
+  return (
+      <Routes>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+  );
 }
 
 export default App;
