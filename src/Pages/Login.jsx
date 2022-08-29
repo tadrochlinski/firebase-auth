4fd2bcc90746db061b473664cf3fbce7004e2d77
tadrochlinski <tadrochlinski@gmail.com>
2022-08-29 23:28:47 +0200

    login page html coded

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () =>{
   return (
-    <div>Login</div>
+    <>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="email"/>
+        <input type="password" placeholder="password"/>
+        <div>
+          <Link to="../register">Create account</Link>
+          <button>Log In</button>
+        </div>
+      </form>
+    </>
   )
 }
 
