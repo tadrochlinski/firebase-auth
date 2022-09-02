de1745a14c9412612904300ecf9668b625dc32e1
tadrochlinski <tadrochlinski@gmail.com>
2022-09-02 14:58:48 +0200

    current user email displays after signup

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 import {
     createUserWithEmailAndPassword, 
     signInWithEmailAndPassword, 
@@ -10,12 +10,23 @@ import { auth } from '../firebase'
 const UserContext = createContext()
 
 export const AuthContextProvider = ({ children }) =>{
+    const [user, setUser] = useState();
+
     const createUser = (email, password) =>{
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    useEffect(() =>{
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) =>{
+            console.log(currentUser)
+            setUser(currentUser)
+        })
+
+        return () => unsubscribe()
+    },[])
+
     return(
-        <UserContext.Provider value={{ createUser }}>
+        <UserContext.Provider value={{ createUser, user }}>
             {children}
         </UserContext.Provider>
     )
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Button from '../../Components/Button'
 import PageWrapper from '../../Components/PageWrapper'
+import { UserAuth } from '../../Context/AuthContext'
 
 const Home = () =>{
+  const {user, logout} = UserAuth()
+
   return (
     <PageWrapper>
       <div>
         <h1>Home</h1>
         <form>
-          <p>email@email.com</p>
+          <p>{user && user.email}</p>
           <Button>Log out</Button>
         </form>
       </div>
